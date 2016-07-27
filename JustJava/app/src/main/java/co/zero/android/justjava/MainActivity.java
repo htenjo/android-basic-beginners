ac4bafb4a6bdd046750a35a90015faad4b7752ab
Hernan Tenjo <zerovirus23@gmail.com>
2016-07-27 10:52:01 -0500

    Working with checkboxes

--- JustJava/app/src/main/java/co/zero/android/justjava/MainActivity.java
+++ JustJava/app/src/main/java/co/zero/android/justjava/MainActivity.java
@@ -4,6 +4,7 @@ package co.zero.android.justjava;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.CheckBox;
 import android.widget.TextView;
 
 import java.text.NumberFormat;
@@ -12,6 +13,8 @@ public class MainActivity extends AppCompatActivity {
     private int cupsAmount = 0;
     private int currentTurn = 0;
     private int cupPrice = 5;
+    private boolean whippedCream;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,7 +27,7 @@ public class MainActivity extends AppCompatActivity {
      */
     public void submitOrder(View view) {
         currentTurn++;
-        displayTurn();
+        displayOrderInfo();
     }
 
     public void increaseAmount(View view){
@@ -52,12 +55,34 @@ public class MainActivity extends AppCompatActivity {
 
     private void displayPrice(){
         TextView quantityTextView = (TextView) findViewById(R.id.price_text_view);
-        String price = NumberFormat.getCurrencyInstance().format(cupsAmount * cupPrice);
+        String price = NumberFormat.getCurrencyInstance().format(getTotalPrice());
         quantityTextView.setText(price);
     }
 
-    private void displayTurn(){
-        TextView turnView = (TextView) findViewById(R.id.turn_text_view);
-        turnView.setText("Your turn is: " + currentTurn);
+    private int getTotalPrice(){
+        return cupsAmount * cupPrice;
+    }
+
+    public void selectWhippedCream(View view){
+        CheckBox checkBox = (CheckBox) view;
+        whippedCream = checkBox.isChecked();
+    }
+
+    private void displayOrderInfo(){
+        TextView turnView = (TextView) findViewById(R.id.order_text_view);
+
+        StringBuffer orderInfo = new StringBuffer();
+        orderInfo.append("===============================");
+        orderInfo.append("\nORDER SUMMARY");
+        orderInfo.append("\n===============================");
+        orderInfo.append("\nName: Hernán Tenjo");
+        orderInfo.append("\nQuantity: " + this.cupsAmount);
+        orderInfo.append("\nWhit Whipped Cream? : " + this.whippedCream);
+        orderInfo.append("\nTotal: " + NumberFormat.getCurrencyInstance().format(getTotalPrice()));
+        orderInfo.append("\nThank you!!!");
+        orderInfo.append("\nYour turn is: " + currentTurn);
+        orderInfo.append("\n=============================");
+
+        turnView.setText(orderInfo.toString());
     }
 }
